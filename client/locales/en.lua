02c5391afa7f95e2cc4de5fd845ec14e91490f75
5mdevsupport <98621363+5mdevsupport@users.noreply.github.com>
2022-04-22 23:15:51 +0200

    Add files via upload

---
+++ client/locales/en.lua
@@ -0,0 +1,9 @@
+i18n.importData("en", {
+	taxi_contact = "Taxi bellen",
+	welcome_message = "Je kunt via je telefoon de taxi bellen.",
+	dispatch_message = "Uw taxi is onderweg!",
+	drivers_busy = "Al onze chaffeurs zijn op dit moment bezig. Probeer het later opnieuw.",
+	info_message = "Press ~INPUT_PICKUP~ to drive as a passenger or ~INPUT_ENTER~ to steal the vehicle.",
+	info_waypoint_message = "Selecteer een bestemming op de kaart en druk daarna op ~INPUT_PICKUP~ om te vertrekken.",
+	taxi_dispatch = "De taxi is naar u onderweg!"
+})
